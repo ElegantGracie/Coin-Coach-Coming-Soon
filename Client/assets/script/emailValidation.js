5bd67d5a6a9e4ff211766b668f169b14b625cc5c
Elegant-Gracie <gracenwafor50@gmail.com>
2023-02-20 16:35:39 +0100

    progress

--- Client/assets/script/emailValidation.js
+++ Client/assets/script/emailValidation.js
@@ -1,45 +1,35 @@
-function confirm(){
-    Swal.fire(
-        'Mail sent!',
-        'Check your inbox, spam or promotions.',
-        'success'
-    )    
-}
-
-function notSent(){
-    Swal.fire(
-        'Mail invalid!',
-        'Fill the correct mail',
-        // 'success'
-      )    
-}
-
+// Function to validate email and send response to backend
 function EmailValidation(enteredEmail) {
+    // Email validation
     let mail_format = /^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/;
 
     if(enteredEmail.value.match(mail_format)) {
 
-        Swal.fire(
-            'Mail sent!',
-            'Check your inbox, spam or promotions.',
-            'success'
-        ) 
-
-        // document.form1.text1.focus();
-
-        return true;
-    } else {
-
-        notSent()
-
-        // document.form1.text1.focus();
-
-        return false;
-    }
-
+        axios.post('http://localhost:5500', {
+            enteredEmail: enteredEmail.value
+        }).then(()=> {
+            // Sweet alert popup if successful 
+            Swal.fire(
+                'Mail sent!',
+                'Check your inbox, spam or promotions.',
+                'success'
+            )
+        }).catch(()=> {
+            // Sweet alert popup if not successful
+            Swal.fire(
+                'Mail invalid!',
+                'Fill the correct mail'
+            )
+        })        
+    } 
 }
 
-let submit = document.getElementById('button-addon2');
-let email = document.getElementById('email');
+// Form data
+const form = getElementById('message-form')
 
-submit.addEventListener('click', EmailValidation(email));
+// EventListener to send email address
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let email = new FormData(form);
+    EmailValidation(email);
+});
