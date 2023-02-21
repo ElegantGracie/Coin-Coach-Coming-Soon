6f0e8f99fc1a12a7f5550bce81a12920f467e280
Elegant-Gracie <gracenwafor50@gmail.com>
2023-02-21 03:34:35 +0100

    :(

--- Client/assets/script/emailValidation.js
+++ Client/assets/script/emailValidation.js
@@ -1,20 +1,22 @@
 // Function to validate email and send response to backend
-function EmailValidation(enteredEmail) {
+function EmailValidation(email) {
     // Email validation
-    let mail_format = /^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/;
+    let mail_format = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
 
-    if(enteredEmail.value.match(mail_format)) {
+    if(email.match(mail_format)) {
 
-        axios.post('http://localhost:5500', {
-            enteredEmail: enteredEmail.value
-        }).then(()=> {
+        axios.post('', {
+            enteredEmail: email
+        }).then((response)=> {
+            console.log(response.data)
             // Sweet alert popup if successful 
             Swal.fire(
                 'Mail sent!',
                 'Check your inbox, spam or promotions.',
                 'success'
-            )
-        }).catch(()=> {
+            );
+        }).catch((error)=> {
+            console.log(error)
             // Sweet alert popup if not successful
             Swal.fire(
                 'Mail invalid!',
@@ -25,11 +27,12 @@ function EmailValidation(enteredEmail) {
 }
 
 // Form data
-const form = getElementById('message-form')
+const form = document.getElementById('message-form');
+
 
 // EventListener to send email address
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    let email = new FormData(form);
+    let email = form.email.value;
     EmailValidation(email);
 });
--- Server/index.js
+++ Server/index.js
@@ -1,49 +1,64 @@
 // Dependencies
 const express = require('express')
 const cors = require('cors')
+const bodyParser = require('body-parser')
 const nodemailer = require('nodemailer')
+const axios = require('axios')
+const cheerio = require('cheerio')
 require('dotenv').config()
 
 // Instance of express
-const app  = express()
+const app = express()
 
 // Port
-const port = process.env.PORT
+const port = process.env.URL || 3000
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(cors({origin: '*'}))
 
-// Function to send mail
-function sendEmail() {
-    return new Promise((resolve, reject) =>  {
-        var transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: process.env.USER,
-                pass: process.env.PASS
+axios.get(port)
+  .then((response) => {
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const email = $('#email').val();
+
+    // Function to send mail
+    function sendEmail() {
+        return new Promise((resolve, reject) =>  {
+            var transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                    user: process.env.USER,
+                    pass: process.env.PASS
+                }
+            })
+            const mail_configs = {
+                from: process.env.USER,
+                to: email,
+                subject: 'Testing',
+                text: 'Welcome'
             }
+            transporter.sendMail(mail_configs, function(error, info) {
+                if(error) {
+                    console.log(error);
+                    return reject({message: 'Not sent'});
+                }
+                return resolve({message: 'Sent'})
+            })
         })
-        const mail_configs = {
-            from: process.env.USER,
-            to: email,
-            subject: 'Testing',
-            text: 'Welcome'
-        }
-        transporter.sendMail(mail_configs, function(error, info) {
-            if(error) {
-                console.log(error);
-                return reject({message: 'Not sent'});
-            }
-            return resolve({message: 'Sent'})
-        })
+    }
+
+    app.get('/send', (req, res) => {
+        sendEmail()
+        .then((response) => res.send(response.message))
+        .catch((error) => res.status(500).send(error.message))
     })
-}
 
-app.get('/send', (req, res) => {
-    sendEmail()
-    .then((res) => res.send(response, message))
-    .catch((error) => res.status(500).send(error.message))
-})
-
-app.listen(port => {
-    console.log(`App is listening at ${port}`)
-})
+    app.listen(url, () => {
+        console.log(`App is listening at ${port}`)
+    })
+  })
+  .catch((error) => {
+    console.log(error);
+  });
